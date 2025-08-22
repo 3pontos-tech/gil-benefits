4742c02b81eb5601f54b773ca59d9caa4ddb5091
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-08-22 18:35:57 -0300

    test(Dashboard): browser tests for dashboard page

---
+++ tests/E2E/Admin/Page/DashboardTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Companies\Company;
+use App\Models\Users\User;
+
+use function Pest\Laravel\actingAs;
+
+it('should load the statuses on dashboard page', function (): void {
+    $admin = User::factory()->admin()->create();
+    Company::factory()->recycle($admin)->create();
+    actingAs($admin);
+
+    $page = visit('/admin');
+    $page->assertSee('Active Plans 0 Current active plans');
+    $page->assertSee('New Users 1 This week');
+    $page->assertSee('Total Companies 1 Overall');
+});
+it('should list latest companies on the chart', function (): void {
+    $admin = User::factory()->admin()->create();
+    Company::factory()->recycle($admin)->create();
+    $companies = Company::factory(9)->create();
+    actingAs($admin);
+
+    $page = visit('/admin');
+
+    $companies->each(function ($company) use ($page) {
+        $page->assertSee($company->name);
+        $page->assertSee($company->tax_id);
+    });
+});
