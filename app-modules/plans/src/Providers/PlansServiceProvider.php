8c6d8c956b4cfaef3609d0bd6888c75341096ce8
Paula Araujo <p.araujop13@gmail.com>
2025-10-02 18:54:17 -0300

    feat: set plans and vouchers service providers

--- app-modules/plans/src/Providers/PlansServiceProvider.php
+++ app-modules/plans/src/Providers/PlansServiceProvider.php
@@ -2,11 +2,31 @@
 
 namespace TresPontosTech\Plans\Providers;
 
+use Filament\Panel;
 use Illuminate\Support\ServiceProvider;
 
 class PlansServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        Panel::configureUsing(function (Panel $panel): void {
+            if ($panel->getId() === 'admin') {
+                $panel
+                    ->discoverResources(
+                        in: __DIR__ . '/../Filament/Admin/Resources',
+                        for: 'TresPontosTech\\Plans\\Filament\\Admin\\Resources'
+                    )
+                    ->discoverWidgets(
+                        in: __DIR__ . '/../Filament/Admin/Widgets',
+                        for: 'TresPontosTech\\Plans\\Filament\\Admin\\Widgets'
+                    )
+                    ->discoverPages(
+                        in: __DIR__ . '/../Filament/Admin/Pages',
+                        for: 'TresPontosTech\\Plans\\Filament\\Admin\\Pages'
+                    );
+            }
+        });
+    }
 
     public function boot(): void {}
 }
--- app-modules/vouchers/src/Providers/VouchersServiceProvider.php
+++ app-modules/vouchers/src/Providers/VouchersServiceProvider.php
@@ -2,11 +2,47 @@
 
 namespace TresPontosTech\Vouchers\Providers;
 
+use Filament\Panel;
 use Illuminate\Support\ServiceProvider;
 
 class VouchersServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        Panel::configureUsing(function (Panel $panel): void {
+            if ($panel->getId() === 'admin') {
+                $panel
+                    ->discoverResources(
+                        in: __DIR__ . '/../Filament/Admin/Resources',
+                        for: 'TresPontosTech\\Vouchers\\Filament\\Admin\\Resources'
+                    )
+                    ->discoverWidgets(
+                        in: __DIR__ . '/../Filament/Admin/Widgets',
+                        for: 'TresPontosTech\\Vouchers\\Filament\\Admin\\Widgets'
+                    )
+                    ->discoverPages(
+                        in: __DIR__ . '/../Filament/Admin/Pages',
+                        for: 'TresPontosTech\\Vouchers\\Filament\\Admin\\Pages'
+                    );
+            }
+
+            if ($panel->getId() === 'company') {
+                $panel
+                    ->discoverResources(
+                        in: __DIR__ . '/../Filament/Company/Resources',
+                        for: 'TresPontosTech\\Vouchers\\Filament\\Company\\Resources'
+                    )
+                    ->discoverWidgets(
+                        in: __DIR__ . '/../Filament/Company/Widgets',
+                        for: 'TresPontosTech\\Vouchers\\Filament\\Company\\Widgets'
+                    )
+                    ->discoverPages(
+                        in: __DIR__ . '/../Filament/Company/Pages',
+                        for: 'TresPontosTech\\Vouchers\\Filament\\Company\\Pages'
+                    );
+            }
+        });
+    }
 
     public function boot(): void {}
 }
