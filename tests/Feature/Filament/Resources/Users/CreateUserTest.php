f452db25601ce1d3766749e461bc63f081c8d80b
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-10-17 14:48:58 -0300

    test: user resource

---
+++ tests/Feature/Filament/Resources/Users/CreateUserTest.php
@@ -0,0 +1,93 @@
+<?php
+
+use App\Filament\Admin\Resources\Users\Pages\CreateUser;
+use App\Filament\FilamentPanel;
+use App\Models\Users\Detail;
+use App\Models\Users\User;
+use Filament\Facades\Filament;
+use TresPontosTech\Company\Models\Company;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Livewire\livewire;
+
+beforeEach(function () {
+    actingAs(User::factory()->admin()->create());
+    Filament::setCurrentPanel(FilamentPanel::Admin->value);
+});
+
+it('should render', function (): void {
+    livewire(CreateUser::class)
+        ->assertOk();
+});
+
+it('should be able to register an user', function () {
+    $company = Company::factory()->createOne();
+
+    livewire(CreateUser::class)
+        ->assertOk()
+        ->fillForm([
+            'name' => 'John Doe',
+            'email' => 'joe@doe.com',
+            'password' => 'password',
+            'detail.tax_id' => '999.999.999-99',
+            'detail.document_id' => '99.999.999-9',
+            'detail.company_id' => $company->getKey(),
+
+        ])
+        ->call('create')
+        ->assertHasNoFormErrors();
+
+    assertDatabaseHas(User::class, [
+        'name' => 'John Doe',
+        'email' => 'joe@doe.com',
+    ]);
+    $user = User::query()->where('users.email', 'joe@doe.com')->first();
+
+    assertDatabaseHas(Detail::class, [
+        'user_id' => $user->getKey(),
+        'tax_id' => '999.999.999-99',
+        'document_id' => '99.999.999-9',
+        'company_id' => $company->getKey(),
+    ]);
+});
+
+describe('validation tests', function () {
+
+    test('name field', function ($value, $rule): void {
+        livewire(CreateUser::class)
+            ->assertOk()
+            ->fillForm([
+                'name' => $value,
+            ])
+            ->call('create')
+            ->assertHasFormErrors(['name' => $rule]);
+    })->with([
+        'required' => ['', 'required'],
+    ]);
+
+    test('email field', function ($value, $rule): void {
+        livewire(CreateUser::class)
+            ->assertOk()
+            ->fillForm([
+                'email' => $value,
+            ])
+            ->call('create')
+            ->assertHasFormErrors(['email' => $rule]);
+    })->with([
+        'required' => ['', 'required'],
+        'email' => ['not-a-email', 'email'],
+    ]);
+
+    test('password field', function ($value, $rule): void {
+        livewire(CreateUser::class)
+            ->assertOk()
+            ->fillForm([
+                'password' => $value,
+            ])
+            ->call('create')
+            ->assertHasFormErrors(['password' => $rule]);
+    })->with([
+        'required' => ['', 'required'],
+    ]);
+});
---
+++ tests/Feature/Filament/Resources/Users/ListUsersTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Filament\Admin\Resources\Users\Pages\ListUsers;
+use App\Filament\FilamentPanel;
+use App\Models\Users\User;
+use Filament\Facades\Filament;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
+
+beforeEach(function () {
+    actingAs(User::factory()->admin()->create());
+    Filament::setCurrentPanel(FilamentPanel::Admin->value);
+});
+
+it('should render', function (): void {
+    livewire(ListUsers::class)
+        ->assertOk();
+});
+
+it('should render all the users', function () {
+    $users = User::factory()->count(8)->create();
+    livewire(ListUsers::class)
+        ->assertOk()
+        ->assertCanSeeTableRecords($users)
+        ->assertCountTableRecords(9);
+});
