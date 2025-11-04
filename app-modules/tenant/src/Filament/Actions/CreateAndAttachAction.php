65f4cddeae6b376daa577b922ae26d88cb0483ca
danielhe4rt <danielhe4rt@gmail.com>
2025-11-04 18:59:28 -0300

    feat: add Filament actions for tenant seat management and employee creation
    - Introduced `TenantSeatsCounterAction` for displaying active tenant seat count.
    - Added `CreateAndAttachAction` to enable tenant employee creation and attachment.

---
+++ app-modules/tenant/src/Filament/Actions/CreateAndAttachAction.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace TresPontosTech\Tenant\Filament\Actions;
+
+use Filament\Actions\CreateAction;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Grid;
+use Hash;
+use Laravel\Cashier\Subscription;
+use TresPontosTech\Company\Models\Company;
+
+class CreateAndAttachAction extends CreateAction
+{
+    public static function getDefaultName(): ?string
+    {
+        return 'create-and-attach-tenant-employee';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->disabled(fn () => $this->isSubscriptionCapacityExceeded());
+
+        $this->after(
+            fn ($record) => filament()->getTenant()->employees()->attach($record, ['role' => 'employee'])
+        );
+
+        $this->schema($this->buildFormSchema());
+    }
+
+    private function buildFormSchema(): array
+    {
+        return [
+            Hidden::make('company_id')->default(filament()->getTenant()->getKey()),
+            TextInput::make('name'),
+            TextInput::make('email')
+                ->rules(['email', 'unique:users,email']),
+            TextInput::make('password')
+                ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                ->password(),
+            Grid::make(1)
+                ->relationship('detail')
+                ->schema([
+                    Hidden::make('company_id')->default(filament()->getTenant()->getKey()),
+                    TextInput::make('tax_id'),
+                    TextInput::make('document_id'),
+                    TextInput::make('phone_number'),
+                ]),
+        ];
+    }
+
+    private function isSubscriptionCapacityExceeded(): bool
+    {
+
+        /** @var Company $tenant */
+        $tenant = filament()->getTenant();
+
+        /** @var Subscription $activeSubscription */
+        $activeSubscription = $tenant->subscriptions()->where('stripe_status', '=', 'active')->first();
+
+        $employeesCount = $tenant->employees()->wherePivot('active', true)->count();
+
+        return $activeSubscription->quantity <= $employeesCount;
+    }
+}
