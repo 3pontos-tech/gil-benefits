050350159aea748893ed670d53ba498b7a906af6
kaster <diogokaster@gmail.com>
2025-11-04 13:41:07 -0300

    feat: add a text-image-right section and update the landing page layout

--- resources/views/filament/guest/pages/landing-page.blade.php
+++ resources/views/filament/guest/pages/landing-page.blade.php
@@ -5,4 +5,5 @@
     <x-sections.horizontal-header-with-grid />
     <x-sections.horizontal-header-with-grid-surface-bg />
     <x-sections.text-image-left />
+    <x-sections.text-image-right />
 </x-filament-panels::page>
