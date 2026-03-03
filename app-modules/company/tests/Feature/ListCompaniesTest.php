78d34067ceb047e188b4127d7c42e4300b9aa200
RichardGL11 <richardgreghilammenda@hotmail.com>
2026-03-03 16:23:03 -0300

    test: company resource

---
+++ app-modules/company/tests/Feature/ListCompaniesTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use TresPontosTech\Company\Filament\Admin\Resources\Companies\Pages\ListCompanies;
+use TresPontosTech\Company\Models\Company;
+
+use function Pest\Livewire\livewire;
+
+beforeEach(function (): void {
+    actingAsAdmin();
+});
+
+it('should render', function (): void {
+    livewire(ListCompanies::class)
+        ->assertOk();
+});
+
+it('should list all companies', function (): void {
+    $companies = Company::factory()->count(10)->create();
+    livewire(ListCompanies::class)
+        ->assertOk()
+        ->assertCanSeeTableRecords($companies);
+
+});
