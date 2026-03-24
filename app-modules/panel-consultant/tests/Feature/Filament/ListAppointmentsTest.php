0d4bce5a64dd873f386c4387e21d86015ff21b80
RichardGL11 <richardgreghilammenda@hotmail.com>
2026-03-24 17:45:15 -0300

    chore: rector

--- app-modules/panel-consultant/tests/Feature/Filament/ListAppointmentsTest.php
+++ app-modules/panel-consultant/tests/Feature/Filament/ListAppointmentsTest.php
@@ -10,7 +10,7 @@ use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 use function Pest\Livewire\livewire;
 
-beforeEach(function () {
+beforeEach(function (): void {
     $this->consultant = actingAsConsultant();
 });
 it('should render', function (): void {
@@ -26,7 +26,7 @@ it("should render only consultant's appointments", function (): void {
         ->assertCanNotSeeTableRecords($anotherAppointments);
 });
 
-test('normal users can not see consultant dashboard', function () {
+test('normal users can not see consultant dashboard', function (): void {
     actingAs(User::factory()->createOne());
 
     get(route('filament.consultant.pages.consultant-dashboard'))
