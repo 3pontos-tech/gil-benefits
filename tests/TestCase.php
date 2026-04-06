81a5351de5f83bb6715dafea63a64627b2ec2b57
RichardGL11 <richardgreghilammenda@hotmail.com>
2026-04-06 17:23:13 -0300

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,16 @@
 
 namespace Tests;
 
+use Database\Seeders\PermissionSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Storage;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected bool $seed = true;
+
+    protected string $seeder = PermissionSeeder::class;
+
     protected function setUp(): void
     {
         parent::setUp();
