780a7fc987406410e9da6c905bdfbfec096222fe
Paula Araujo <p.araujop13@gmail.com>
2026-04-22 16:01:58 -0300

    feat: user import modal with clearer instructions

--- app-modules/user/src/Http/Controllers/DownloadImportTemplateController.php
+++ app-modules/user/src/Http/Controllers/DownloadImportTemplateController.php
@@ -2,14 +2,20 @@
 
 namespace TresPontosTech\User\Http\Controllers;
 
-use Symfony\Component\HttpFoundation\StreamedResponse;
+use Spatie\SimpleExcel\SimpleExcelWriter;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DownloadImportTemplateController
 {
-    public function __invoke(): StreamedResponse
+    public function __invoke(): BinaryFileResponse
     {
-        return response()->streamDownload(function (): void {
-            echo implode(',', ['name', 'email', 'phone_number', 'document_id', 'tax_id']) . "\n";
-        }, 'template-import-users.csv');
+        $tempPath = tempnam(sys_get_temp_dir(), 'template_') . '.xlsx';
+
+        SimpleExcelWriter::create($tempPath)
+            ->addHeader(['name', 'email', 'phone_number', 'document_id', 'tax_id'])
+            ->addRow(['João Silva', 'joaosilva@email.com', '11987654321', '12345678', '12345678901'])
+            ->close();
+
+        return response()->download($tempPath, 'template-import-users.xlsx')->deleteFileAfterSend(true);
     }
 }
