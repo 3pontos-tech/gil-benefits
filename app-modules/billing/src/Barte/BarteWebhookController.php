86f9db4c195397ad09e383b8afdecdef74ee9e95
RichardGL11 <richardgreghilammenda@hotmail.com>
2026-04-28 15:51:44 -0300

    wip

--- app-modules/billing/src/Barte/BarteWebhookController.php
+++ app-modules/billing/src/Barte/BarteWebhookController.php
@@ -6,20 +6,14 @@ namespace TresPontosTech\Billing\Barte;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Log;
-use TresPontosTech\Billing\Barte\Actions\HandleBarteWebhook;
-use TresPontosTech\Billing\Barte\DTOs\BarteWebhookDto;
+use TresPontosTech\Billing\Barte\Jobs\HandleBarteWebhookJob;
 
 class BarteWebhookController extends Controller
 {
-    public function __construct(private readonly HandleBarteWebhook $action) {}
-
     public function handle(Request $request): void
     {
         $payload = $request->all();
 
-        Log::info('Barte webhook recebido', $payload);
-
-        $this->action->handle(BarteWebhookDto::fromArray($payload));
+        HandleBarteWebhookJob::dispatch($payload);
     }
 }
