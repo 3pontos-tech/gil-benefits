5b44154d75ba0adb79a3ff4b07a97961ebde8777
Thales Machado <102062680+thalesmengue@users.noreply.github.com>
2026-05-12 13:20:30 -0300

    feat(users): dispatch welcome email with password when user is create… (#182)
    …d via admin/company panels

--- app-modules/panel-admin/src/Filament/Resources/Users/Pages/CreateUser.php
+++ app-modules/panel-admin/src/Filament/Resources/Users/Pages/CreateUser.php
@@ -4,13 +4,29 @@ namespace TresPontosTech\Admin\Filament\Resources\Users\Pages;
 
 use Filament\Resources\Pages\CreateRecord;
 use TresPontosTech\Admin\Filament\Resources\Users\UserResource;
+use TresPontosTech\Permissions\Roles;
+use TresPontosTech\User\Events\UserRegistered;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
 
+    protected ?string $plainPassword = null;
+
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $this->plainPassword = $data['password'] ?? null;
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        event(new UserRegistered($this->record, Roles::User, $this->plainPassword));
     }
 }
